798bcc2d6855e6e51c9257e65759b0adf48a774f
ShavindiLiyanage <ssanthupamaliyanage@gmail.com>
2026-01-17 15:11:27 +0530

    #Implement pw encryption and loggers

---
+++ LawMateBackend/LawMate.API/Controllers/Common/EncryptionController.cs
@@ -0,0 +1,35 @@
+﻿using LawMate.API.Model.Common;
+using LawMate.Application.Common.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LawMate.API.Controllers.Common
+{
+    [Route("api/encryption")]
+    [ApiController]
+    public class EncryptionController : ControllerBase
+    {
+        [HttpPost("password-encrypt")]
+        public IActionResult EncryptPassword([FromBody] LoginRequest request)
+        {
+            if (string.IsNullOrEmpty(request.Password) || string.IsNullOrEmpty(request.UserId))
+            {
+                return BadRequest("PlainText and UserId cannot be empty.");
+            }
+
+            string encryptedText = CryptoUtil.Encrypt(request.Password, request.UserId);
+            return Ok(new { encryptedPassword = encryptedText });
+        }
+
+        [HttpPost("password-decrypt")]
+        public IActionResult DecryptPassword([FromBody] LoginRequest request)
+        {
+            if (string.IsNullOrEmpty(request.Password) || string.IsNullOrEmpty(request.UserId))
+            {
+                return BadRequest("EncryptedPassword and UserId cannot be empty.");
+            }
+
+            string decryptedText = CryptoUtil.Decrypt(request.Password, request.UserId);
+            return Ok(new { decryptedPassword = decryptedText });
+        }
+    }
+}
