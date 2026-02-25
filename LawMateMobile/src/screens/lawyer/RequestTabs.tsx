9f5313ff15f8c28a0999c48ba6f64a7e632f71ab
Ishara <namal.i@informaticsint.com>
2026-02-25 15:19:45 +0530

    create appoinment view screens

--- LawMateMobile/src/screens/lawyer/RequestTabs.tsx
+++ LawMateMobile/src/screens/lawyer/RequestTabs.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { spacing, borderRadius, colors, fontWeight } from '../../config/theme';
 
 interface Props {
   activeTab: 'Pending' | 'Confirmed' | 'Rejected';
