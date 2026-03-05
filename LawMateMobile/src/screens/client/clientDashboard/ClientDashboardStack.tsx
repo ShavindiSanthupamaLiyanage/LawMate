7c47c6926b22f305b2f01fde31870e6f305d00c1
NelunikaNimalrathna <devindi.nelu@gmail.com>
2026-03-05 13:36:06 +0530

    Client-Client Dashboard chat bot configuration

--- LawMateMobile/src/screens/client/clientDashboard/ClientDashboardStack.tsx
+++ LawMateMobile/src/screens/client/clientDashboard/ClientDashboardStack.tsx
@@ -3,10 +3,12 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import ClientDashboard from "./ClientDashboard";
 import ClientActivityList from "./ClientActivityList";
+import ClientChatbotScreen from "../clientChatbot/ClientChatbotScreen";
 
 export type ClientDashboardStackParamList = {
     ClientDashboardHome: undefined;
     ClientActivityList: undefined;
+    ClientChatbot: undefined;
 };
 
 const Stack = createNativeStackNavigator<ClientDashboardStackParamList>();
@@ -16,6 +18,7 @@ export default function ClientDashboardStack() {
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="ClientDashboardHome" component={ClientDashboard} />
             <Stack.Screen name="ClientActivityList" component={ClientActivityList} />
+            <Stack.Screen name="ClientChatbot" component={ClientChatbotScreen} />
         </Stack.Navigator>
     );
 }
