dc34e73bcefdcb4f88043231015595f6516eeca6
Gihan Kanishka <gihankanishka123abc@gmail.com>
2026-03-08 19:47:10 +0530

    Add GetAllArticles and GetArticlesByLawyer queries and update Article entity/DTO
    - Made Content property required in Article entity
    - Created ArticleDto
    - Implemented GetAllArticlesQuery
    - Implemented GetArticlesByLawyerQuery
    - Created ArticleController for fetching articles

---
+++ LawMateBackend/LawMate.Application/LawyerModule/LawyerKnowledgeHub/Queries/GetArticlesByLawyerQuery.cs
@@ -0,0 +1,48 @@
+﻿using LawMate.Application.Common.Interfaces;
+using LawMate.Domain.DTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace LawMate.Application.LawyerModule.LawyerKnowledgeHub.Queries
+{
+    public record GetArticlesByLawyerQuery(string LawyerId) : IRequest<List<ArticleDto>>;
+
+    public class GetArticlesByLawyerQueryHandler
+        : IRequestHandler<GetArticlesByLawyerQuery, List<ArticleDto>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetArticlesByLawyerQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ArticleDto>> Handle(
+            GetArticlesByLawyerQuery request,
+            CancellationToken cancellationToken)
+        {
+            return await _context.ARTICLE
+                .Where(a => a.LawyerId == request.LawyerId) // now both are string
+                .OrderByDescending(a => a.CreatedAt)
+                .Select(a => new ArticleDto
+                {
+                    ArticleId = a.ArticleId,
+                    LawyerId = a.LawyerId,
+                    Title = a.Title,
+                    Content = a.Content,
+                    LegalCategory = a.LegalCategory.ToString(),
+                    Language = a.Language.ToString(),
+                    IsPublished = a.IsPublished,
+                    CreatedBy = a.CreatedBy,
+                    CreatedAt = a.CreatedAt,
+                    ModifiedBy = a.ModifiedBy,
+                    ModifiedAt = a.ModifiedAt,
+                })
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
