af8f2e8c5dedc98b70ee1cb576bcec807dde2bf7
Gihan Kanishka <gihankanishka123abc@gmail.com>
2026-03-08 20:31:27 +0530

    Delete,Update and related COntroller changes

---
+++ LawMateBackend/LawMate.Application/LawyerModule/LawyerKnowledgeHub/Command/DeleteArticleCommand.cs
@@ -0,0 +1,33 @@
+﻿using LawMate.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LawMate.Application.LawyerModule.LawyerKnowledgeHub.Command
+{
+    public record DeleteArticleCommand(string ArticleId) : IRequest<bool>;
+
+    public class DeleteArticleCommandHandler : IRequestHandler<DeleteArticleCommand, bool>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteArticleCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(DeleteArticleCommand request, CancellationToken cancellationToken)
+        {
+            var article = await _context.ARTICLE
+                .FirstOrDefaultAsync(a => a.ArticleId == request.ArticleId, cancellationToken);
+
+            if (article == null)
+                throw new Exception($"Article with ID {request.ArticleId} not found.");
+
+            _context.ARTICLE.Remove(article);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true; // successfully deleted
+        }
+    }
+}
