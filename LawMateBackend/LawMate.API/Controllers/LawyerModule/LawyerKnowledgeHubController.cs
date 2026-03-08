7dd1372de7a9f3e7c327dea7c87a3f1e477748aa
Gihan Kanishka <gihankanishka123abc@gmail.com>
2026-03-08 19:47:10 +0530

    Add GetAllArticles and GetArticlesByLawyer queries and update Article entity/DTO
    - Made Content property required in Article entity
    - Created ArticleDto
    - Implemented GetAllArticlesQuery
    - Implemented GetArticlesByLawyerQuery
    - Created ArticleController for fetching articles

---
+++ LawMateBackend/LawMate.API/Controllers/LawyerModule/LawyerKnowledgeHubController.cs
@@ -0,0 +1,47 @@
+﻿using LawMate.Application.LawyerModule.LawyerKnowledgeHub.Command;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using LawMate.Application.LawyerModule.LawyerKnowledgeHub.Queries;
+using LawMate.Domain.DTOs;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LawMate.API.Controllers.LawyerModule;
+
+[ApiController]
+[Route("api/lawyers/articles")]
+public class ArticleController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ArticleController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    //Get all articles 
+    [Authorize]
+    [HttpGet("articles")]
+    public async Task<IActionResult> GetAllArticles()
+    {
+        var result = await _mediator.Send(new GetAllArticlesQuery());
+        return Ok(result);
+    }
+
+    //Get the articles which related to a specific lawyer
+    [Authorize]
+    [HttpGet("lawyer/{lawyerId}")]
+    public async Task<IActionResult> GetArticlesByLawyer(string lawyerId)
+    {
+        var result = await _mediator.Send(new GetArticlesByLawyerQuery(lawyerId));
+        return Ok(result);
+    }
+    
+    //Create a new article
+    [Authorize]
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateArticle([FromBody] ArticleDto articleDto)
+    {
+        var result = await _mediator.Send(new CreateArticleCommand(articleDto));
+        return CreatedAtAction(nameof(GetArticlesByLawyer), new { lawyerId = result.LawyerId }, result);
+    }
+}
