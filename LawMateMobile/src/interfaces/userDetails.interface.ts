da360f36218c510b0a1e434e4ec5f84234eaca83
ShavindiLiyanage <ssanthupamaliyanage@gmail.com>
2026-03-13 04:24:26 +0530

    Add user counts endpoint and update UserDetailService; refactor UserManagementScreen to display user statistics

---
+++ LawMateMobile/src/interfaces/userDetails.interface.ts
@@ -0,0 +1,8 @@
+export interface UserCountsDto {
+    verifiedLawyers: number;
+    pendingLawyers: number;
+    inactiveLawyers: number;
+    activeLawyers: number;
+    activeClients: number;
+    inactiveClients: number;
+}
