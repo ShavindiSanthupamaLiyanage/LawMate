aafc8c243385cfa5a4eeb0b4a602a20077862ac5
ShavindiLiyanage <ssanthupamaliyanage@gmail.com>
2026-03-15 14:27:42 +0530

    Add payment maintenance functionality with query and command handlers for membership and booking payments

--- LawMateBackend/LawMate.Domain/Common/Enums/AuthEnums.cs
+++ LawMateBackend/LawMate.Domain/Common/Enums/AuthEnums.cs
@@ -148,4 +148,10 @@ namespace LawMate.Domain.Common.Enums
         Monthly = 0,
         BiAnnual = 1,
     }
+    
+    public enum PaymentSource
+    {
+        Membership,
+        Booking
+    }
 }
