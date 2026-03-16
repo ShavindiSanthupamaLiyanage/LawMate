07037c21417c1da066c0d11941414f83c5ee4158
ShavindiLiyanage <ssanthupamaliyanage@gmail.com>
2026-03-16 20:36:56 +0530

    Add unit tests for ContactUsController to validate contact message sending functionality

---
+++ LawMateBackend/LawMate.Tests/Controllers/ClientModule/ContactUsControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using LawMate.API.Controllers.ClientModule;
+using LawMate.Application.ClientModule.ContactUs.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace LawMate.Tests.Controllers.ClientModule
+{
+    public class ContactUsControllerTests
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly ContactUsController _controller;
+
+        public ContactUsControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new ContactUsController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task SendContactMessage_Should_Return_Ok_When_Command_Succeeds()
+        {
+            // Arrange
+            var command = new SendContactUsMessageCommand
+            {
+                FullName = "John Doe",
+                Email = "john@test.com",
+                Message = "Hello support"
+            };
+
+            _mediatorMock
+                .Setup(m => m.Send(It.IsAny<SendContactUsMessageCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.SendContactMessage(command);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+
+            var okResult = result as OkObjectResult;
+
+            okResult!.Value.Should().BeEquivalentTo(new
+            {
+                message = "Your message has been sent successfully."
+            });
+
+            _mediatorMock.Verify(
+                m => m.Send(command, It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+    }
+}
