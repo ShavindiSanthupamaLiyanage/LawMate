e9e8a8a984d263f9c3486f83f823fe1876b9c339
NelunikaNimalrathna <devindi.nelu@gmail.com>
2026-03-17 20:58:20 +0530

    Merge development into feature/client-dashboard-backend and resolve conflicts

--- LawMateBackend/LawMate.API/Controllers/AdminModule/PaymentMaintenanceController.cs
+++ LawMateBackend/LawMate.API/Controllers/AdminModule/PaymentMaintenanceController.cs
@@ -4,7 +4,6 @@ using LawMate.Domain.Common.Enums;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using LawMate.Application.AdminModule.PaymentMaintenance.Commands;
 
 namespace LawMate.API.Controllers.AdminModule;
 
@@ -71,17 +70,6 @@ public class PaymentMaintenanceController : ControllerBase
     public async Task<IActionResult> UpdateBookingPayment(UpdateBookingPaymentStatusCommand command)
     {
         var result = await _mediator.Send(command);
-        return Ok(result);
-    }
-    // Devindi
-    [HttpPost("booking/{paymentId}/mark-paid")]
-    public async Task<IActionResult> MarkBookingPaymentAsPaid(int paymentId)
-    {
-        var result = await _mediator.Send(new MarkBookingPaymentAsPaidCommand
-        {
-            PaymentId = paymentId
-        });
-
         return Ok(result);
     }
 }
