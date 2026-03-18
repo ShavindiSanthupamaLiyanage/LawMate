e503862fceb24f43da18a9d2cee09b33f916338a
BenuriChathurya <pabchathurya05@gmail.com>
2026-03-18 22:13:13 +0530

    Admin dashboard controller created

---
+++ LawMateBackend/LawMate.API/Controllers/AdminModule/AdminDashboardController.cs
@@ -0,0 +1,27 @@
+﻿using LawMate.Application.AdminModule.AdminDashboard.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LawMate.API.Controllers.AdminModule
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/admin-dashboard")]
+    public class AdminDashboardController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AdminDashboardController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDashboard()
+        {
+            var result = await _mediator.Send(new GetAdminDashboardQuery());
+            return Ok(result);
+        }
+    }
+}
