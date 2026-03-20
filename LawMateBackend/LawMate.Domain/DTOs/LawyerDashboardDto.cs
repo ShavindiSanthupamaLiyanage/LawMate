1f750406c6368a108152d39a7794e7304e336d33
Gihan Kanishka <gihankanishka123abc@gmail.com>
2026-03-21 04:13:58 +0530

    Lawyer dashboard BE implementation

---
+++ LawMateBackend/LawMate.Domain/DTOs/LawyerDashboardDto.cs
@@ -0,0 +1,15 @@
+﻿namespace LawMate.Domain.DTOs.Lawyer
+{
+    public class LawyerDashboardDto
+    {
+        public LawyerDashboardSummaryDto Summary { get; set; }
+        public List<AppointmentBreakdownDto> AppointmentBreakdown { get; set; }
+        public List<LawyerActivityDto> RecentActivities { get; set; }
+    }
+
+    public class LawyerDashboardSummaryDto
+    {
+        public int TotalAppointments { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+}
